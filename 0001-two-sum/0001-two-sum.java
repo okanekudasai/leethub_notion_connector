0c8503688a658adcf801d4e6de6e6eb9fe1b1688
이동하 <89767982+okanekudasai@users.noreply.github.com>
2024-05-27 23:51:18 +0900

    Time: 4 ms (62.65%), Space: 44.5 MB (80.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,6 +5,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
         }
+        
         for (int i = 0; i < nums.length; i++) {
             if (map.keySet().contains(target - nums[i]) && i != map.get(target - nums[i])) {
                 answer[0] = i;
