ba9f26bbd275ec78490e8f1a3f34abc172cb3dd6
이동하 <89767982+okanekudasai@users.noreply.github.com>
2024-05-28 06:04:27 +0900

    Time: 4 ms (14%), Space: 41.4 MB (29.59%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        int count;
+        int answer = -1;///
+        for (int i = 0; i <= 1000; i++) {//
+
+
+            count = 0;/*vfvfvfvfvfvfvfvfv*/
+            for (int n : nums) {
+                if (n >= i) count++;
+            }
+            if (count == i) answer = i;
+        }
+        return answer;
+    }
+}
