4536dec18506551f086f181f00d41c53687c6b1e
이동하 <89767982+okanekudasai@users.noreply.github.com>
2024-05-28 04:55:37 +0900

    Time: 4 ms (14%), Space: 41.4 MB (21.8%) - LeetHub

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        int count;
+        int answer = -1;
+        for (int i = 0; i <= 1000; i++) {
+            count = 0;
+            for (int n : nums) {
+                if (n >= i) count++;
+            }
+            if (count == i) answer = i;
+        }
+        return answer;
+    }
+}
