bcabf04609ffbffbdf058d57e83e7d557b73c903
이동하 <89767982+okanekudasai@users.noreply.github.com>
2024-05-31 13:04:44 +0900

    Time: 4 ms (39.6%), Space: 45.3 MB (90.35%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (set.contains(i)) set.remove(i);
+            else set.add(i);
+        }
+        int [] answer = new int [2];
+        int index = 0;
+        for (int i : set) {
+            answer[index++] = i;
+        }
+        return answer;
+    }
+}
