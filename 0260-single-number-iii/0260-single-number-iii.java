97326dab5ef168c1a952a094c8c120849af0e8ae
이동하 <89767982+okanekudasai@users.noreply.github.com>
2024-05-31 13:32:50 +0900

    Time: 4 ms (39.6%), Space: 45.3 MB (81.95%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -5,10 +5,7 @@ class Solution {
             if (set.contains(i)) set.remove(i);
             else set.add(i);
         }
-        ///
-        //848484
-        // asfefaes
-        //959595959
+        //
         int [] answer  = new int [2];
         int index = 0;
         for (int i : set) {
