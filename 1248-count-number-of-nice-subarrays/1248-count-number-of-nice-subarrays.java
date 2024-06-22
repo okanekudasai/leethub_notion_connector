5a34e4d1e43a1910d9d06fd97def8db56fc38930
이동하 <89767982+okanekudasai@users.noreply.github.com>
2024-06-22 16:24:45 +0900

    Time: 33 ms (20.39%), Space: 54.7 MB (71.73%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        int count = 0;
+        for (int i : nums) {
+            if (i % 2 == 0) count++;
+            else {
+                list.add(count);
+                count = 0;
+            }
+        }
+        list.add(count);
+        int index = 0;
+        int answer = 0;
+        int N = list.size();
+        while (true) {
+            if (index+k >= N) break;
+            answer += (list.get(index)+1) * (list.get(index+k)+1);
+            index++;
+        }
+        System.out.println(list);
+        return answer;
+    }
+}
+
+/*
+11121211
+11011
+
+
+00010010001000100
+00011122223333444
+3-2-3-3-2
+
+001100
+
+2-0-2
+*/
